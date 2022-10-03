95bf87cfd9eb6b967c2109cce1b9b82b09c3dec8
Ash <ashjehov@gmail.com>
2022-10-04 09:09:53 +1000

    Assignment 4, Demo 1 and Demo 2 updates.

---
+++ View/js/components.js
@@ -0,0 +1,21 @@
+function caseComponents() {
+    return`<div class="case-container">
+            <div class="image-video-container">
+                <div class="user-profile-wrapper">
+                    <div class="business-profile-pic">
+                        <object class="case-image" data="../View/uploads/pexels-elvis-2528118.jpg"></object>
+                    </div>
+                    <div class="business-name">
+                        XYZ Marketing solutions
+                    </div>
+                    <div class="date-posted">
+                        5 days ago
+                    </div>
+                </div>
+                <object class="case-image" data="../View/uploads/pexels-elvis-2528118.jpg"></object>
+            </div>
+        <div class="case-instructions">
+            <span class="case-subject">Menu not working properly</span>
+        </div>
+    </div>`
+}
